585bfc0d8bfae6c971ef9686dba99c4f69689d37
lyh7712 <mmj2rv9@kakao.com>
2021-05-03 20:21:00 +0900

    Initial commit

---
+++ src/test/java/lyh7712/springmvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package lyh7712.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
